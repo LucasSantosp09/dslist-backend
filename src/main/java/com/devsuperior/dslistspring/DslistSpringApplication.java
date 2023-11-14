1fdd72d027fb3e80b9360d50f884cfc919f111ba
LucasSantosp09 <lucassantos.p09@gmail.com>
2023-11-14 14:45:25 -0300

    Project Created

---
+++ src/main/java/com/devsuperior/dslistspring/DslistSpringApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslistspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistSpringApplication.class, args);
+	}
+
+}
